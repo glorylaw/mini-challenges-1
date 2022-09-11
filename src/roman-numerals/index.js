bf03fa5c33481dd61b33e27b569c9bb2a02e1667
glorylaw <100371169+glorylaw@users.noreply.github.com>
2022-09-11 16:35:42 +0100

    done

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,29 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    let romanNumbers={
+        M:1000,
+        D:500,
+        C:100,
+        L:50,
+        X:10,
+        V:5,
+        I:1
+    }
+    let result=0
+
+    for(let i= 0;i< roman.length;i++){
+        let presentRoman=roman[i]
+        let nextRoman=roman[i +1]
+        if(romanNumbers[nextRoman]> romanNumbers[presentRoman]){
+            result+=romanNumbers[nextRoman]-romanNumbers[presentRoman]
+            i++
+        } else{
+            result+=romanNumbers[presentRoman]
+        }
+    }
+    return result
+}
 
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -1,9 +1,23 @@
+const { isFunctionExpression } = require("typescript");
+
 /**
  * Implement the solution in this function
  *
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let result =[]
+    
+    for(let i=0;i<array[0].length;i++){
+        let newArray =[]
+        for(let k=0;k<array.length;k++){
+            newArray.push(array[k][i])
+        }
+       result.push(newArray) 
+    }
+    return result
+}
+
 
 module.exports = transpose;
